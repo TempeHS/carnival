30de836517e4d8259089d1da69953af470f96ec7
sayelDot <50289498+sayelDot@users.noreply.github.com>
2019-05-07 11:26:07 +1000

    Added calculations for height of results graphs

--- script2.js
+++ script2.js
@@ -12,10 +12,25 @@ function callXml() {
   xhttp.send();
 };
 function myFunction(xml) {
+  
+var bScore = Number(xml.responseXML.getElementsByTagName("BRADMAN")[0].childNodes[0].nodeValue);
+var pScore = Number(xml.responseXML.getElementsByTagName("PERKINS")[0].childNodes[0].nodeValue);
+var sScore = Number(xml.responseXML.getElementsByTagName("SAUVAGE")[0].childNodes[0].nodeValue);
+var fScore = Number(xml.responseXML.getElementsByTagName("FREEMAN")[0].childNodes[0].nodeValue);
+  
+var graphTotal = bScore + pScore + sScore + fScore;
+  
+document.getElementById("bGraph").style.height = bScore / graphTotal * 150;
+document.getElementById("pGraph").style.height = pScore / graphTotal * 150;
+document.getElementById("sGraph").style.height = sScore / graphTotal * 150;
+document.getElementById("fGraph").style.height = fScore / graphTotal * 150;
 
-document.getElementById("bradmanScore").innerHTML = xml.responseXML.getElementsByTagName("BRADMAN")[0].childNodes[0].nodeValue;
-document.getElementById("perkinsScore").innerHTML = xml.responseXML.getElementsByTagName("PERKINS")[0].childNodes[0].nodeValue;
-document.getElementById("sauvageScore").innerHTML = xml.responseXML.getElementsByTagName("SAUVAGE")[0].childNodes[0].nodeValue;
-document.getElementById("freemanScore").innerHTML = xml.responseXML.getElementsByTagName("FREEMAN")[0].childNodes[0].nodeValue;
+document.getElementById("bradmanScore").innerHTML = bScore;
+document.getElementById("perkinsScore").innerHTML = pScore;
+document.getElementById("sauvageScore").innerHTML = sScore;
+document.getElementById("freemanScore").innerHTML = fScore;
+  
+
+
 }
 setInterval( callXml, 1000);
