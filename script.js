bbc816b6d015927a8d83fc09c2630f6d91d5a03b
sayelDot <50289498+sayelDot@users.noreply.github.com>
2019-05-09 15:31:46 +1000

    Added XML sources for all HTML block ids

--- script.js
+++ script.js
@@ -18,6 +18,11 @@ var pScore = Number(xml.responseXML.getElementsByTagName("PERKINS")[0].childNode
 var sScore = Number(xml.responseXML.getElementsByTagName("SAUVAGE")[0].childNodes[0].nodeValue);
 var fScore = Number(xml.responseXML.getElementsByTagName("FREEMAN")[0].childNodes[0].nodeValue);
   
+document.getElementById("rh").innerHTML = xml.responseXML.getElementsByTagName("RH")[0].childNodes[0].nodeValue;
+document.getElementById("r1").innerHTML = xml.responseXML.getElementsByTagName("R1")[0].childNodes[0].nodeValue;
+document.getElementById("r2").innerHTML = xml.responseXML.getElementsByTagName("R2")[0].childNodes[0].nodeValue;
+document.getElementById("r3").innerHTML = xml.responseXML.getElementsByTagName("R3")[0].childNodes[0].nodeValue;
+  
 var graphTotal = bScore + pScore + sScore + fScore;
 var bScoreMath = bScore / graphTotal * 300;
 var pScoreMath = pScore / graphTotal * 300;
@@ -38,9 +43,33 @@ bScore = xml.responseXML.getElementsByTagName("BRADMAN")[0].childNodes[0].nodeVa
 pScore = xml.responseXML.getElementsByTagName("PERKINS")[0].childNodes[0].nodeValue;
 sScore = xml.responseXML.getElementsByTagName("SAUVAGE")[0].childNodes[0].nodeValue;
 fScore = xml.responseXML.getElementsByTagName("FREEMAN")[0].childNodes[0].nodeValue;
-document.getElementById("cb").innerHTML = xml.responseXML.getElementsByTagName("CB")[0].childNodes[0].nodeValue;
-document.getElementById("nb").innerHTML = xml.responseXML.getElementsByTagName("NB")[0].childNodes[0].nodeValue;
-document.getElementById("ce").innerHTML = xml.responseXML.getElementsByTagName("CETL1")[0].childNodes[0].nodeValue;
+
+var i = 0;
+
+document.getElementById("cb").innerHTML = xml.responseXML.getElementsByTagName("CB")[i].childNodes[i].nodeValue;
+document.getElementById("nb").innerHTML = xml.responseXML.getElementsByTagName("NB")[i].childNodes[i].nodeValue;
+document.getElementById("cetl1").innerHTML = xml.responseXML.getElementsByTagName("CETL1")[i].childNodes[i].nodeValue;
+document.getElementById("cetl2").innerHTML = xml.responseXML.getElementsByTagName("CETL2")[i].childNodes[i].nodeValue;
+document.getElementById("cetl3").innerHTML = xml.responseXML.getElementsByTagName("CETL3")[i].childNodes[i].nodeValue;
+  
+document.getElementById("cefl1").innerHTML = xml.responseXML.getElementsByTagName("CEFL1")[i].childNodes[i].nodeValue;
+document.getElementById("cefl2").innerHTML = xml.responseXML.getElementsByTagName("CEFL2")[i].childNodes[i].nodeValue;
+document.getElementById("cefl3").innerHTML = xml.responseXML.getElementsByTagName("CEFL3")[i].childNodes[i].nodeValue;
+document.getElementById("cefl4").innerHTML = xml.responseXML.getElementsByTagName("CEFL4")[i].childNodes[i].nodeValue;
+document.getElementById("cefl5").innerHTML = xml.responseXML.getElementsByTagName("CEFL5")[i].childNodes[i].nodeValue;
+document.getElementById("cefl6").innerHTML = xml.responseXML.getElementsByTagName("CEFL6")[i].childNodes[i].nodeValue;
+document.getElementById("cefl7").innerHTML = xml.responseXML.getElementsByTagName("CEFL7")[i].childNodes[i].nodeValue;
+  
+document.getElementById("netl1").innerHTML = xml.responseXML.getElementsByTagName("NETL1")[i].childNodes[i].nodeValue;
+document.getElementById("netl2").innerHTML = xml.responseXML.getElementsByTagName("NETL2")[i].childNodes[i].nodeValue;
+document.getElementById("netl3").innerHTML = xml.responseXML.getElementsByTagName("NETL3")[i].childNodes[i].nodeValue;
+document.getElementById("nefl1").innerHTML = xml.responseXML.getElementsByTagName("NEFL1")[i].childNodes[i].nodeValue;
+document.getElementById("nefl2").innerHTML = xml.responseXML.getElementsByTagName("NEFL2")[i].childNodes[i].nodeValue;
+document.getElementById("nefl3").innerHTML = xml.responseXML.getElementsByTagName("NEFL3")[i].childNodes[i].nodeValue;
+document.getElementById("nefl4").innerHTML = xml.responseXML.getElementsByTagName("NEFL4")[i].childNodes[i].nodeValue;
+document.getElementById("nefl5").innerHTML = xml.responseXML.getElementsByTagName("NEFL5")[i].childNodes[i].nodeValue;
+document.getElementById("nefl6").innerHTML = xml.responseXML.getElementsByTagName("NEFL6")[i].childNodes[i].nodeValue;
+document.getElementById("nefl7").innerHTML = xml.responseXML.getElementsByTagName("NEFL7")[i].childNodes[i].nodeValue;
   
 
 }
