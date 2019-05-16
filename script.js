3c02f8dd428acec40b89023d7c74c773f034fbf6
Ben Jones <benpaddlejones@outlook.com>
2019-05-16 15:48:20 +1000

    fixed nodevalue error

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var i = 0;
+var i = 5;
+var x = 0;
 callXml();
 
 
@@ -15,15 +16,15 @@ function callXml() {
 };
 function myFunction(xml) {
   
-var bScore = Number(xml.responseXML.getElementsByTagName("BRADMAN")[0].childNodes[0].nodeValue);
-var pScore = Number(xml.responseXML.getElementsByTagName("PERKINS")[0].childNodes[0].nodeValue);
-var sScore = Number(xml.responseXML.getElementsByTagName("SAUVAGE")[0].childNodes[0].nodeValue);
-var fScore = Number(xml.responseXML.getElementsByTagName("FREEMAN")[0].childNodes[0].nodeValue);
+var bScore = Number(xml.responseXML.getElementsByTagName("BRADMAN")[x].childNodes[x].nodeValue);
+var pScore = Number(xml.responseXML.getElementsByTagName("PERKINS")[x].childNodes[x].nodeValue);
+var sScore = Number(xml.responseXML.getElementsByTagName("SAUVAGE")[x].childNodes[x].nodeValue);
+var fScore = Number(xml.responseXML.getElementsByTagName("FREEMAN")[x].childNodes[x].nodeValue);
   
-document.getElementById("rh").innerHTML = xml.responseXML.getElementsByTagName("RH")[0].childNodes[0].nodeValue;
-document.getElementById("r1").innerHTML = xml.responseXML.getElementsByTagName("R1")[0].childNodes[0].nodeValue;
-document.getElementById("r2").innerHTML = xml.responseXML.getElementsByTagName("R2")[0].childNodes[0].nodeValue;
-document.getElementById("r3").innerHTML = xml.responseXML.getElementsByTagName("R3")[0].childNodes[0].nodeValue;
+document.getElementById("rh").innerHTML = xml.responseXML.getElementsByTagName("RH")[x].childNodes[x].nodeValue;
+document.getElementById("r1").innerHTML = xml.responseXML.getElementsByTagName("R1")[x].childNodes[x].nodeValue;
+document.getElementById("r2").innerHTML = xml.responseXML.getElementsByTagName("R2")[x].childNodes[x].nodeValue;
+document.getElementById("r3").innerHTML = xml.responseXML.getElementsByTagName("R3")[x].childNodes[x].nodeValue;
   
 var graphTotal = bScore + pScore + sScore + fScore;
 var bScoreMath = bScore / graphTotal * 300;
@@ -41,56 +42,56 @@ document.getElementById("perkinsScore").innerHTML = pScore;
 document.getElementById("sauvageScore").innerHTML = sScore;
 document.getElementById("freemanScore").innerHTML = fScore;
   
-bScore = xml.responseXML.getElementsByTagName("BRADMAN")[0].childNodes[0].nodeValue;
-pScore = xml.responseXML.getElementsByTagName("PERKINS")[0].childNodes[0].nodeValue;
-sScore = xml.responseXML.getElementsByTagName("SAUVAGE")[0].childNodes[0].nodeValue;
-fScore = xml.responseXML.getElementsByTagName("FREEMAN")[0].childNodes[0].nodeValue;
+bScore = xml.responseXML.getElementsByTagName("BRADMAN")[x].childNodes[x].nodeValue;
+pScore = xml.responseXML.getElementsByTagName("PERKINS")[x].childNodes[x].nodeValue;
+sScore = xml.responseXML.getElementsByTagName("SAUVAGE")[x].childNodes[x].nodeValue;
+fScore = xml.responseXML.getElementsByTagName("FREEMAN")[x].childNodes[x].nodeValue;
 
 //year, month 0-11, date, hour, min
-if(new Date(2019, 4, 15, 10, 25).getTime() < Date.now()) {
+if(new Date(2019, 4, 24, 10, 25).getTime() < Date.now()) {
 i = 1;
 }
 //year, month 0-11, date, hour, min
-if(new Date(2019, 4, 15, 11, 35).getTime() < Date.now()) {
+if(new Date(2019, 4, 24, 11, 35).getTime() < Date.now()) {
 i = 2;
 }
 //year, month 0-11, date, hour, min
-if(new Date(2019, 4, 15, 12, 25).getTime() < Date.now()) {
+if(new Date(2019, 4, 24, 12, 25).getTime() < Date.now()) {
 i = 3;
 }
 //year, month 0-11, date, hour, min
-if(new Date(2019, 4, 15, 13, 15).getTime() < Date.now()) {
+if(new Date(2019, 4, 24, 13, 15).getTime() < Date.now()) {
 i = 4;
 }
 //year, month 0-11, date, hour, min
-if(new Date(2019, 4, 15, 14, 5).getTime() < Date.now()) {
+if(new Date(2019, 4, 24, 14, 5).getTime() < Date.now()) {
 i = 5;
 }
 
-document.getElementById("cb").innerHTML = xml.responseXML.getElementsByTagName("CB")[i].childNodes[i];
-document.getElementById("nb").innerHTML = xml.responseXML.getElementsByTagName("NB")[i].childNodes[i];
-document.getElementById("cetl1").innerHTML = xml.responseXML.getElementsByTagName("CETL1")[i].childNodes[i];
-document.getElementById("cetl2").innerHTML = xml.responseXML.getElementsByTagName("CETL2")[i].childNodes[i];
-document.getElementById("cetl3").innerHTML = xml.responseXML.getElementsByTagName("CETL3")[i].childNodes[i];
+document.getElementById("cb").innerHTML = xml.responseXML.getElementsByTagName("CB")[i].childNodes[x].nodeValue;
+document.getElementById("nb").innerHTML = xml.responseXML.getElementsByTagName("NB")[i].childNodes[x].nodeValue;
+document.getElementById("cetl1").innerHTML = xml.responseXML.getElementsByTagName("CETL1")[i].childNodes[x].nodeValue;
+document.getElementById("cetl2").innerHTML = xml.responseXML.getElementsByTagName("CETL2")[i].childNodes[x].nodeValue;
+document.getElementById("cetl3").innerHTML = xml.responseXML.getElementsByTagName("CETL3")[i].childNodes[x].nodeValue;
   
-document.getElementById("cefl1").innerHTML = xml.responseXML.getElementsByTagName("CEFL1")[i].childNodes[i];
-document.getElementById("cefl2").innerHTML = xml.responseXML.getElementsByTagName("CEFL2")[i].childNodes[i];
-document.getElementById("cefl3").innerHTML = xml.responseXML.getElementsByTagName("CEFL3")[i].childNodes[i];
-document.getElementById("cefl4").innerHTML = xml.responseXML.getElementsByTagName("CEFL4")[i].childNodes[i];
-document.getElementById("cefl5").innerHTML = xml.responseXML.getElementsByTagName("CEFL5")[i].childNodes[i].nodeValue;
-document.getElementById("cefl6").innerHTML = xml.responseXML.getElementsByTagName("CEFL6")[i].childNodes[i].nodeValue;
-document.getElementById("cefl7").innerHTML = xml.responseXML.getElementsByTagName("CEFL7")[i].childNodes[i].nodeValue;
+document.getElementById("cefl1").innerHTML = xml.responseXML.getElementsByTagName("CEFL1")[i].childNodes[x].nodeValue;
+document.getElementById("cefl2").innerHTML = xml.responseXML.getElementsByTagName("CEFL2")[i].childNodes[x].nodeValue;
+document.getElementById("cefl3").innerHTML = xml.responseXML.getElementsByTagName("CEFL3")[i].childNodes[x].nodeValue;
+document.getElementById("cefl4").innerHTML = xml.responseXML.getElementsByTagName("CEFL4")[i].childNodes[x].nodeValue;
+document.getElementById("cefl5").innerHTML = xml.responseXML.getElementsByTagName("CEFL5")[i].childNodes[x].nodeValue;
+document.getElementById("cefl6").innerHTML = xml.responseXML.getElementsByTagName("CEFL6")[i].childNodes[x].nodeValue;
+document.getElementById("cefl7").innerHTML = xml.responseXML.getElementsByTagName("CEFL7")[i].childNodes[x].nodeValue;
   
-document.getElementById("netl1").innerHTML = xml.responseXML.getElementsByTagName("NETL1")[i].childNodes[i].nodeValue;
-document.getElementById("netl2").innerHTML = xml.responseXML.getElementsByTagName("NETL2")[i].childNodes[i].nodeValue;
-document.getElementById("netl3").innerHTML = xml.responseXML.getElementsByTagName("NETL3")[i].childNodes[i].nodeValue;
-document.getElementById("nefl1").innerHTML = xml.responseXML.getElementsByTagName("NEFL1")[i].childNodes[i].nodeValue;
-document.getElementById("nefl2").innerHTML = xml.responseXML.getElementsByTagName("NEFL2")[i].childNodes[i].nodeValue;
-document.getElementById("nefl3").innerHTML = xml.responseXML.getElementsByTagName("NEFL3")[i].childNodes[i].nodeValue;
-document.getElementById("nefl4").innerHTML = xml.responseXML.getElementsByTagName("NEFL4")[i].childNodes[i].nodeValue;
-document.getElementById("nefl5").innerHTML = xml.responseXML.getElementsByTagName("NEFL5")[i].childNodes[i].nodeValue;
-document.getElementById("nefl6").innerHTML = xml.responseXML.getElementsByTagName("NEFL6")[i].childNodes[i].nodeValue;
-document.getElementById("nefl7").innerHTML = xml.responseXML.getElementsByTagName("NEFL7")[i].childNodes[i].nodeValue;
+document.getElementById("netl1").innerHTML = xml.responseXML.getElementsByTagName("NETL1")[i].childNodes[x].nodeValue;
+document.getElementById("netl2").innerHTML = xml.responseXML.getElementsByTagName("NETL2")[i].childNodes[x].nodeValue;
+document.getElementById("netl3").innerHTML = xml.responseXML.getElementsByTagName("NETL3")[i].childNodes[x].nodeValue;
+document.getElementById("nefl1").innerHTML = xml.responseXML.getElementsByTagName("NEFL1")[i].childNodes[x].nodeValue;
+document.getElementById("nefl2").innerHTML = xml.responseXML.getElementsByTagName("NEFL2")[i].childNodes[x].nodeValue;
+document.getElementById("nefl3").innerHTML = xml.responseXML.getElementsByTagName("NEFL3")[i].childNodes[x].nodeValue;
+document.getElementById("nefl4").innerHTML = xml.responseXML.getElementsByTagName("NEFL4")[i].childNodes[x].nodeValue;
+document.getElementById("nefl5").innerHTML = xml.responseXML.getElementsByTagName("NEFL5")[i].childNodes[x].nodeValue;
+document.getElementById("nefl6").innerHTML = xml.responseXML.getElementsByTagName("NEFL6")[i].childNodes[x].nodeValue;
+document.getElementById("nefl7").innerHTML = xml.responseXML.getElementsByTagName("NEFL7")[i].childNodes[x].nodeValue;
   
 
 }
